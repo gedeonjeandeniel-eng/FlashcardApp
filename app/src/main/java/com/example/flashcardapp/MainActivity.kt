8fb85c6d3404ca4b5645ba115d439fad5e1e23be
Jean Deniel GEDEON <gedeonjeandeniel@gmail.com>
2025-09-05 17:02:50 -0400

    Initial commit of FlashcardApp2 project

---
+++ app/src/main/java/com/example/flashcardapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.flashcardapp
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val flashcardQuestion= findViewById<TextView>(R.id.Flashcard_question)
+        val flashcardAnswer = findViewById<TextView>(R.id.Flashcard_Answer)
+        flashcardQuestion.setOnClickListener {
+            flashcardQuestion.visibility = View.INVISIBLE
+            flashcardAnswer.visibility = View.VISIBLE
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
