b8a63dbc9040334a65053c5a9420875665af08c5
Jean Deniel GEDEON <gedeonjeandeniel@gmail.com>
2025-10-03 12:07:27 -0400

    ajoute yon bouton ki pemet itilizate a ajoute nouvo tach

--- app/src/main/java/com/example/flashcardapp/AddCardActivity.kt
+++ app/src/main/java/com/example/flashcardapp/AddCardActivity.kt
@@ -1,26 +1,16 @@
 package com.example.flashcardapp
 
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class AddCardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        val flashcardQuestion= findViewById<TextView>(R.id.Flashcard_question)
-        val flashcardAnswer = findViewById<TextView>(R.id.Flashcard_Answer)
-        flashcardQuestion.setOnClickListener {
-            flashcardQuestion.visibility = View.INVISIBLE
-            flashcardAnswer.visibility = View.VISIBLE
-
-        }
-
+        setContentView(R.layout.addcardactivity)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
